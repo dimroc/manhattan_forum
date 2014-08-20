2cc9a15554e12d7c4a006189543184de3ffb5ed8
Dimitri Roche <dimroc@gmail.com>
2014-08-19 23:13:34 -0400

    barebones functionality to navigate from boroughs to topics

---
+++ ManhattanForum/TopicsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  TopicsViewController.swift
+//  ManhattanForum
+//
+//  Created by Dimitri Roche on 8/19/14.
+//  Copyright (c) 2014 dimroc. All rights reserved.
+//
+
+import UIKit
+
+class TopicsViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
