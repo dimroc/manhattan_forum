ec0d7d1ae5755a81090438c24f01110724db5297
Dimitri Roche <dimroc@gmail.com>
2014-08-21 09:07:42 -0400

    Add Create Topic View Controller and Dismiss Segue

---
+++ ManhattanForum/CreateTopicViewController.swift
@@ -0,0 +1,12 @@
+//
+//  CreateTopicViewController.swift
+//  ManhattanForum
+//
+//  Created by Dimitri Roche on 8/21/14.
+//  Copyright (c) 2014 dimroc. All rights reserved.
+//
+
+import UIKit
+
+class CreateTopicViewController: UIViewController {
+}
---
+++ ManhattanForum/DismissSegue.swift
@@ -0,0 +1,18 @@
+//
+//  DismissSegue.swift
+//  ManhattanForum
+//
+//  Created by Dimitri Roche on 8/21/14.
+//  Copyright (c) 2014 dimroc. All rights reserved.
+//
+
+import UIKit
+
+@objc(DismissSegue)
+class DismissSegue: UIStoryboardSegue {
+    override func perform() {
+        let src = self.sourceViewController as UIViewController
+        src.presentingViewController.dismissViewControllerAnimated(true, completion: { () -> Void in
+        })
+    }
+}
