01c39fab06fd0fc9bc8e086c0a6ca3951133f6b0
Dimitri Roche <dimroc@gmail.com>
2014-08-22 07:45:46 -0400

    Set custom font Open Sans to UILabel default

--- ManhattanForum/AppDelegate.swift
+++ ManhattanForum/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication!, didFinishLaunchingWithOptions launchOptions: NSDictionary?) -> Bool {
         // Override point for customization after application launch.
         ParseHelper.launch(launchOptions)
+        FontHelper.launch()
         return true
     }
 
---
+++ ManhattanForum/FontHelper.swift
@@ -0,0 +1,15 @@
+//
+//  FontHelper.swift
+//  ManhattanForum
+//
+//  Created by Dimitri Roche on 8/22/14.
+//  Copyright (c) 2014 dimroc. All rights reserved.
+//
+
+import Foundation
+
+class FontHelper {
+    class func launch() {
+        UILabel.appearance().font = UIFont(name: "Open Sans", size: 16.0)
+    }
+}
