376ab73b348aca4660220da81b49ca77991dbcd2
Dimitri Roche <dimroc@gmail.com>
2014-08-23 14:37:15 -0400

    Add camera action sheet to create topic VC

--- ManhattanForum/CreateTopicViewController.swift
+++ ManhattanForum/CreateTopicViewController.swift
@@ -8,10 +8,29 @@
 
 import UIKit
 
-class CreateTopicViewController: UIViewController {
+class CreateTopicViewController: UIViewController, UIActionSheetDelegate {
     @IBOutlet weak var textView: UITextView!
     
     override func viewDidLoad() {
         textView.becomeFirstResponder()
+    }
+    
+    @IBAction func showCameraActionSheet(AnyObject) {
+        println("button tapped!")
+        let actionSheet = UIActionSheet(title: nil, delegate: self, cancelButtonTitle: "Cancel", destructiveButtonTitle: nil, otherButtonTitles: "Take photo", "Take video", "Choose from library")
+        actionSheet.showInView(view)
+    }
+    
+    func actionSheet(actionSheet: UIActionSheet!, clickedButtonAtIndex buttonIndex: Int) {
+        switch buttonIndex {
+        case 1:
+            println("Take photo")
+        case 2:
+            println("Take video")
+        case 3:
+            println("Choose from library")
+        default:
+            println("Missed option in camera sheet:\(buttonIndex)")
+        }
     }
 }
