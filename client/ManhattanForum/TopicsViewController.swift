0741233a431eeafbc7fdd5598403f26e233185e3
Dimitri Roche <dimroc@gmail.com>
2015-01-02 17:17:26 -0500

    Show empty topics message and enable refresh control

--- client/ManhattanForum/TopicsViewController.swift
+++ client/ManhattanForum/TopicsViewController.swift
@@ -12,6 +12,7 @@ class TopicsViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        refreshControl?.addTarget(self, action: "refresh:", forControlEvents: UIControlEvents.ValueChanged)
         // Do any additional setup after loading the view, typically from a nib.
     }
     
@@ -20,5 +21,29 @@ class TopicsViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // Return the number of sections.
+        if (true) {
+            
+            // Display a message when the table is empty
+            let messageLabel = UILabel(frame: CGRectMake(0, 0, self.view.bounds.size.width, self.view.bounds.size.height))
+            
+            messageLabel.text = "No data is currently available. Please pull down to refresh.";
+            messageLabel.textColor = UIColor.blackColor();
+            messageLabel.numberOfLines = 0;
+            messageLabel.textAlignment = NSTextAlignment.Center;
+            messageLabel.font = UIFont(name: "OpenSans-Regular", size: 16)
+            messageLabel.sizeToFit()
+            
+            self.tableView.backgroundView = messageLabel;
+            self.tableView.separatorStyle = UITableViewCellSeparatorStyle.None;
+        }
+        
+        return 0;
+    }
     
+    @IBAction func refresh(sender: AnyObject) {
+        println("refreshed!!")
+        refreshControl?.endRefreshing()
+    }
 }
