35569ac9faa33c6b4829bce4c876d4708de5a783
Dimitri Roche <dimroc@gmail.com>
2015-01-03 19:27:36 -0500

    Successfully populate table view with posts

---
+++ client/ManhattanForum/PostCell.swift
@@ -0,0 +1,17 @@
+//
+//  PostCell.swift
+//  ManhattanForum
+//
+//  Created by Dimitri Roche on 1/3/15.
+//  Copyright (c) 2015 dimroc. All rights reserved.
+//
+
+import Foundation
+
+class PostCell: UITableViewCell {
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    func populateFromPost(post: PFObject) {
+        self.messageLabel.text = post["message"] as? String
+    }
+}
--- client/ManhattanForum/TopicsViewController.swift
+++ client/ManhattanForum/TopicsViewController.swift
@@ -9,11 +9,14 @@
 import UIKit
 
 class TopicsViewController: UITableViewController {
+    var posts: Array<PFObject> = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         refreshControl?.addTarget(self, action: "refresh:", forControlEvents: UIControlEvents.ValueChanged)
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let nib = UINib(nibName: "PostCell", bundle: nil)
+        self.tableView.registerNib(nib, forCellReuseIdentifier: "PostCell")
     }
     
     override func didReceiveMemoryWarning() {
@@ -23,7 +26,7 @@ class TopicsViewController: UITableViewController {
     
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // Return the number of sections.
-        if (true) {
+        if (self.posts.isEmpty) {
             
             // Display a message when the table is empty
             let messageLabel = UILabel(frame: CGRectMake(0, 0, self.view.bounds.size.width, self.view.bounds.size.height))
@@ -37,14 +40,40 @@ class TopicsViewController: UITableViewController {
             
             self.tableView.backgroundView = messageLabel;
             self.tableView.separatorStyle = UITableViewCellSeparatorStyle.None;
+            return 0;
+        } else {
+            self.tableView.backgroundView = nil;
+            self.tableView.separatorStyle = UITableViewCellSeparatorStyle.SingleLine;
+
+            return 1;
         }
-        
-        return 0;
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.posts.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cellIdentifier = "PostCell"
+        let cell: PostCell! = tableView.dequeueReusableCellWithIdentifier(cellIdentifier) as PostCell
+        cell.populateFromPost(self.posts[indexPath.row])
+        return cell
     }
     
     @IBAction func refresh(sender: AnyObject) {
         PostRepository.retrieveAsync().continueWithBlockOnMain { (task: BFTask!) -> AnyObject! in
-            println("refreshed!!")
+            if (task.success) {
+                self.posts = task.result as Array<PFObject>
+                self.tableView.reloadData()
+            } else {
+                NSLog(task.error.debugDescription)
+                
+                self.presentViewController(
+                    UIAlertControllerFactory.ok("Error refreshing", message: task.error.localizedDescription),
+                    animated: true,
+                    completion: nil)
+            }
+            
             self.refreshControl?.endRefreshing()
             return nil
         }
