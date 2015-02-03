47e033c4491e67c49facded2c00f9e4600031542
Dimitri Roche <dimroc@gmail.com>
2015-02-02 23:37:50 -0500

    Add Cloud Code increment post#comments in afterSave

--- parse/cloud/main.js
+++ parse/cloud/main.js
@@ -1,6 +1,13 @@
-
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+// https://parse.com/docs/cloud_code_guide#functions-aftersave
+Parse.Cloud.afterSave("Comment", function(request) {
+  query = new Parse.Query("Post");
+  query.get(request.object.get("post").id, {
+    success: function(post) {
+      post.increment("comments");
+      post.save();
+    },
+    error: function(error) {
+      console.error("Got an error " + error.code + " : " + error.message);
+    }
+  });
 });
